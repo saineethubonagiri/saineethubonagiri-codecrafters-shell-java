c0d233586ac29c520fbe2fdfda3b77bbb7190f4d
saineethubonagiri <157181471+saineethubonagiri@users.noreply.github.com>
2025-12-12 00:45:37 -0500

    exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
         while(true){
             System.out.print("$ ");
             String input = sc.next();
-            if(input == "exit") {
+            if(input.equals("exit")) {
                 System.exit(0);
 
             }
