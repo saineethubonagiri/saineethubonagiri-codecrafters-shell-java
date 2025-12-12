1b1c9585cf839cfa67b8523a48d8f6c1a0ec2cdf
saineethubonagiri <157181471+saineethubonagiri@users.noreply.github.com>
2025-12-12 14:40:27 -0500

    echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+        //System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
        // String cmd = sc.next();
         //System.out.println(cmd + ": command not found");
 
         while(true){
-           System.out.print("$ ");
+           //System.out.print("$ ");
             String input = sc.nextLine();
             if(input.equals("exit")) {
                 System.exit(0);
