c8c5a3fd03a4a0056f83dc18bef0190c9b455655
saineethubonagiri <157181471+saineethubonagiri@users.noreply.github.com>
2025-12-11 22:56:20 -0500

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.next();
+        System.out.println(cmd + "command not found");
     }
 }
